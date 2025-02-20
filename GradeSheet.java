40145f62792f481533ac825370c5058b7ce3eb04
Nitanshu715 <152681890+Nitanshu715@users.noreply.github.com>
2025-02-20 15:37:13 +0530

    Add files via upload

---
+++ GradeSheet.java
@@ -0,0 +1,23 @@
+public class GradeSheet {
+    public static void main(String[] args) {
+        // Displaying the grade sheet with escape sequences and special characters
+        System.out.println("*********************************************");
+        System.out.println("\t\tGRADE SHEET");
+        System.out.println("*********************************************");
+        System.out.println("*\tName\t\tSubject\t\tGrade\t    *");
+        System.out.println("*********************************************");
+        System.out.println("*\tAlice\t\tMaths\t\tA\t    *");
+        System.out.println("*\tAlice\t\tPhysics\t\tB+\t    *");
+        System.out.println("*\tAlice\t\tChemistry\tA-\t    *");
+        System.out.println("*********************************************");
+        System.out.println("*\tBob\t\tMaths\t\tB\t    *");
+        System.out.println("*\tBob\t\tPhysics\t\tA\t    *");
+        System.out.println("*\tBob\t\tChemistry\tB+\t    *");
+        System.out.println("*********************************************");
+        System.out.println("*\tCharlie\tMaths\t\tA+\t    *");
+        System.out.println("*\tCharlie\tPhysics\t\tA\t    *");
+        System.out.println("*\tCharlie\tChemistry\tA\t    *");
+        System.out.println("*********************************************");
+        System.out.println("\n\t\t\tEND OF GRADE SHEET");
+    }
+}
